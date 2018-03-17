b215255400c47f9555710026de33ab9935cefcaa
unknown <a.larrard@gmail.com>
2018-03-17 14:21:31 +1100

    merge both data

--- script/data_prep/extract_data_atp.py
+++ script/data_prep/extract_data_atp.py
@@ -9,6 +9,9 @@ import pandas as pd
 import glob
 import numpy as np
 from datetime import datetime
+import os
+import re
+import unicodedata
 
 
 def import_data_atp(path):
@@ -24,7 +27,77 @@ def import_data_atp(path):
     data["Date"]   = pd.to_datetime(data["tourney_date"], format = "%Y%m%d")   
     del data["tourney_date"]
     
+    data = data.sort_values(["Date", "tourney_name"])
+    data["ATP_ID"] = range(len(data))
+    
     data.loc[data["winner_name"] == "joshua goodall", "winner_name"] = "josh goodall"
     data.loc[data["loser_name"] == "joshua goodall", "loser_name"] = "josh goodall"
             
     return data.reset_index(drop=True)
+
+
+def merge_match_ID(data1, key):
+    
+    matching_IDS = pd.read_csv(os.environ["DATA_PATH"] + "/clean_datasets/players/match_originID_atpID.csv")
+    datamerge = pd.merge(data1, matching_IDS, on =key, how = "left")
+    
+    print("new data shape is {0}".format(datamerge.shape))
+    
+    return datamerge
+
+
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD',str(s))
+                  if unicodedata.category(c) != 'Mn')
+
+
+def score(x, i, j):
+    try:
+        return re.sub("[\(\[].*?[\)\]]", "", str(x)).split(" ")[i].split("-")[j]
+    except Exception:
+        return "0"
+    
+def compare_score(x):
+    try:
+        return  [int(x[0]), int(x[1]), int(x[2]), int(x[3])] == [x[4], x[5], x[6], x[7]]
+    except Exception:
+        return False
+    
+
+def merge_origin_atp(data_orig, data_atp, common_key = "ATP_ID"):
+    
+    total_data = pd.merge(data_orig, data_atp, on = common_key, how= "left")
+    
+    for col in ["Winner", "Loser" , "winner_name", "loser_name"]:
+        total_data[col] = total_data[col].apply(lambda x : strip_accents(x).replace("'","").replace("-"," ").replace(".","").lstrip().rstrip().lower())
+        
+    total_data["win_lose_orig"] = total_data["Winner"] + " " + total_data["Loser"]
+    total_data["win_lose_atp"] = total_data["winner_name"] + " " + total_data["loser_name"]
+    
+    total_data["bool"] = total_data[["win_lose_orig","win_lose_atp"]].apply(lambda x : len(set.intersection(set(x[0].split(" ")), set(x[1].split(" ")))), axis=1)
+    print("matching results : {0}".format(total_data["bool"].value_counts()))
+    
+#    total_data["score"] = total_data["score"].fillna("0-0 0-0")
+#    total_data["W1_atp"] = total_data["score"].apply(lambda x : score(x, 0, 0))
+#    total_data["L1_atp"] = total_data["score"].apply(lambda x : score(x, 0, 1))
+#    total_data["W2_atp"] = total_data["score"].apply(lambda x : score(x, 1, 0))
+#    total_data["L2_atp"] = total_data["score"].apply(lambda x : score(x, 1, 1))
+#    total_data = total_data.fillna(0)
+#    
+#    total_data.loc[total_data["W1_atp"] == "W/O"] = -1
+#    total_data.loc[total_data["W1_atp"] == ""] = -1
+#    total_data.loc[total_data["L1_atp"] == "W/O"] = -1
+#    total_data.loc[total_data["L1_atp"] == ""] = -1
+#    total_data.loc[total_data["W2_atp"] == "W/O"] = -1
+#    total_data.loc[total_data["W2_atp"] == ""] = -1
+#    total_data.loc[total_data["L2_atp"] == "W/O"] = -1
+#    total_data.loc[total_data["L2_atp"] == ""] = -1
+#    total_data[["W1","W2","L1","L2"]] = total_data[["W1","W2","L1","L2"]].replace("","0").replace(" ", "0").astype(int)
+#    total_data[["W1_atp","W2_atp","L1_atp","L2_atp"]] = total_data[["W1","W2","L1","L2"]].replace("","0").replace(" ", "0").astype(int)
+#    
+#    total_data["bool2"] = total_data[["W1", "W2", "L1","L2", "W1_atp", "W2_atp","L1_atp", "L2_atp"]].apply(lambda x :compare_score(x), axis=1 )
+#    print(total_data["bool2"].value_counts())
+    
+    return total_data
+
+ 
--- script/main.py
+++ script/main.py
@@ -10,7 +10,7 @@ import os
 from modelling.modelling_lr import modelling_logistic
 from data_prep.create_tournament import merge_with_tournois
 from data_prep.extract_data_origin import import_data_origin 
-from data_prep.extract_data_atp import import_data_atp
+from data_prep.extract_data_atp import import_data_atp, merge_match_ID, merge_origin_atp
 from data_prep.create_statistics_history import data_prep_history
 from data_prep.create_elo_ranking import merge_data_elo
 from utils.plot_lib import var_vs_target
@@ -26,8 +26,14 @@ if __name__ == "__main__":
     ### extract statistics and create target variable
     data_origin_tournament = merge_with_tournois(data_origin, os.environ["DATA_PATH"]  + "/clean_datasets/tournament/")
     
+    data_origin_tournament_ID = merge_match_ID(data_origin_tournament, key ="ORIGIN_ID")
+    
     ### read atp data and clean it
     data_atp = import_data_atp(os.environ["DATA_PATH"]  + "/brute_info/historical/brute_info_atp/")
+    
+    #### merge atp origin
+    data_merge = merge_origin_atp(data_origin_tournament_ID, data_atp, common_key = "ATP_ID")
+    
     
     ### add elo system ranking
 #    data_elo = merge_data_elo(data)
